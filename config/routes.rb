b24fcde281ebfc7d23121b053aca4aa3192a0594
oTiagoGs <tiagoguimaraes81@gmail.com>
2024-01-11 18:39:56 -0300

    Create and config home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'site/welcome#index'
 end
